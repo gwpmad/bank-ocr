4a4b948ebbedec99aaadc82090d211baed4fcd31
George Maddocks <gmaddocks1@gmail.com>
2017-03-15 21:51:32 +0000

    Use case 1, opening TDD

---
+++ test/bank-ocr-spec.js
@@ -0,0 +1,19 @@
+const test = require('tape');
+const bankOcr = require('../src/bankOcr');
+const data = require('require-all')(`${__dirname}/data}`);
+
+test('BankOCR use case 1', (t) => {
+	t.equal(bankOcr(data.useCaseOne[0]), 000000000);
+	t.equal(bankOcr(data.useCaseOne[1]), 111111111);
+	t.equal(bankOcr(data.useCaseOne[2]), 222222222);
+	t.equal(bankOcr(data.useCaseOne[3]), 333333333);
+	t.equal(bankOcr(data.useCaseOne[4]), 444444444);
+	t.equal(bankOcr(data.useCaseOne[5]), 555555555);
+	t.equal(bankOcr(data.useCaseOne[6]), 666666666);
+	t.equal(bankOcr(data.useCaseOne[7]), 777777777);
+	t.equal(bankOcr(data.useCaseOne[8]), 888888888);
+	t.equal(bankOcr(data.useCaseOne[9]), 999999999);
+	t.equal(bankOcr(data.useCaseOne[10]), 123456789);
+
+	t.end();
+});
